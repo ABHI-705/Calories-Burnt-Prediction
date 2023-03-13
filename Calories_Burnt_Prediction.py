a55f858de3368d7ed610e65044784cc9c650c37b
ABHI-705 <111434086+ABHI-705@users.noreply.github.com>
2023-03-13 06:19:16 +0530

    Update Calories_Burnt_Prediction.py

--- Calories_Burnt_Prediction.py
+++ Calories_Burnt_Prediction.py
@@ -1,212 +1,64 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import streamlit as st
-from sklearn.model_selection import train_test_split
-from xgboost import XGBRegressor
-from sklearn import metrics
-import warnings
-warnings.filterwarnings('ignore')
-import pickle
+import pandas as pd
+from PIL import Image
 
+st.set_page_config(
+    page_title='Calories Prediction',
+    layout='wide'
+)
 
-# In[2]:
+Calories_model=pickle.load(open('calories.pkl','rb'))
 
+coll1,coll2,coll3=st.columns([2,3,1])
+with coll2:
+    st.title("Calories Burnt Prediction                                         Using ML")
 
-calories=pd.read_csv("C:/Users/tripa/Downloads/archive/calories.csv")
-calories
 
+st.write(" ")
+st.write(" ")
+col1,col2=st.columns(2)
 
-# In[3]:
+with col2:
 
+    gende=st.radio("Select Gender",['Male','Female'])
+    if gende=='Male':
+        gender=0
+    else:
+        gender=1
 
-# print the first 5 rows of the dataframe
-calories.head()
 
 
-# In[4]:
+    Age=st.number_input("Enter the Age")
 
+    Height=st.number_input("Enter the Height")
 
-exercise_data = pd.read_csv("C:/Users/tripa/Downloads/archive/exercise.csv")
 
-exercise_data.head()
 
+    Weight=st.number_input("Enter the weight")
 
-# In[5]:
+    Duration=st.number_input("Enter the Duration of your Exercise")
 
 
-calories_data = pd.concat([exercise_data, calories['Calories']], axis=1)
-calories_data.head()
 
+    Heart_Rate=st.number_input("Enter the Heart Rate")
 
-# In[6]:
 
+    Body_Temp=st.number_input("Enter the Body Temperature")
 
-# checking the number of rows and columns
-calories_data.shape
 
 
-# In[7]:
+    if st.button("Predict Calories Burnt"):
+        input_df = pd.DataFrame({'Gender':[gender],'Age':[Age],'Height':[Height],
+                                'Weight':[Weight],'Duration':[Duration],'Heart_Rate':[Heart_Rate],
+                                'Body_Temp':[Body_Temp]})
+        
+        resul=Calories_model.predict(input_df)
+        
+        result=resul[0]
+        
+        st.header("Burnt calories is :👉 " + str(round(result)))
 
-
-# getting some informations about the data
-calories_data.info()
-
-
-# In[8]:
-
-
-# checking for missing values
-calories_data.isnull().sum()
-
-
-# In[9]:
-
-
-# get some statistical measures about the data
-calories_data.describe()
-
-
-# In[10]:
-
-
-sns.set()
-
-
-# In[11]:
-
-
-# plotting the gender column in count plot
-sns.countplot(calories_data['Gender'])
-
-
-# In[12]:
-
-
-# finding the distribution of "Age" column
-sns.distplot(calories_data['Age'])
-
-
-# In[13]:
-
-
-# finding the distribution of "Height" column
-sns.distplot(calories_data['Height'])
-
-
-# In[14]:
-
-
-# finding the distribution of "Weight" column
-sns.distplot(calories_data['Weight'])
-
-
-# In[15]:
-
-
-correlation = calories_data.corr()
-
-
-# In[16]:
-
-
-# constructing a heatmap to understand the correlation
-
-plt.figure(figsize=(10,10))
-sns.heatmap(correlation, cbar=True, square=True, fmt='.1f', annot=True, annot_kws={'size':8}, cmap='Blues')
-
-
-
-# In[17]:
-
-
-calories_data.replace({"Gender":{'male':0,'female':1}}, inplace=True)
-
-
-# In[18]:
-
-
-calories_data.head()
-
-
-# In[19]:
-
-
-X = calories_data.drop(columns=['User_ID','Calories'], axis=1)
-Y = calories_data['Calories']
-
-print(X)
-print(Y)
-
-
-# In[20]:
-
-
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
-
-
-# In[21]:
-
-
-print(X.shape, X_train.shape, X_test.shape)
-
-
-# In[22]:
-
-
-# loading the model
-model = XGBRegressor()
-
-
-# In[23]:
-
-
-# training the model with X_train
-model.fit(X_train, Y_train)
-
-
-# In[24]:
-
-
-test_data_prediction = model.predict(X_test)
-print(test_data_prediction)
-
-
-# In[25]:
-
-
-mae = metrics.mean_absolute_error(Y_test, test_data_prediction)
-mae
-
-
-# In[26]:
-
-
-filename="calories_model.sav"
-pickle.dump(model, open(filename, 'wb'))
-
-
-# In[27]:
-
-
-loaded_model=pickle.load(open("calories_model.sav","rb"))
-
-
-# In[28]:
-
-
-loaded_model
-
-
-# In[ ]:
-
-
-
-
+with col1:
+    image=Image.open('cal.png')
+    edited=image.resize((2600,1950))
+    st.image(edited,use_column_width=True)
